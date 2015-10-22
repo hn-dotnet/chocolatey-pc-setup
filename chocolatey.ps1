488d6e8a21f75b19804ad3ca1422b1a397c1aaa0
Jonathan Günz <jonathan.guenz@outlook.com>
2015-10-22 18:25:03 +0200

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,20 +1,20 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install powergui
-choco install notepadplusplus
-choco install github
-choco install googlechrome
-choco install paint.net
-choco install teamviewer
-choco install baretail
-choco install gimp
-choco install winrar
-choco install greenshot
-choco install linqpad4
-choco install kdiff3
-choco install rdcman
-choco install firefox
-choco install visualstudio2015community
-choco install sqlserver2014express
-choco install mssqlservermanagementstudio2014express
+choco install powergui -y
+choco install notepadplusplus -y
+choco install github -y
+choco install googlechrome -y
+choco install paint.net -y
+choco install teamviewer -y
+choco install baretail -y
+choco install gimp -y
+choco install winrar -y
+choco install greenshot -y
+choco install linqpad4 -y
+choco install kdiff3 -y
+choco install rdcman -y
+choco install firefox -y
+choco install visualstudio2015community -y
+choco install sqlserver2014express -y
+choco install mssqlservermanagementstudio2014express -y
